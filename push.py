eaa1af9d7ef2598d1230926cc192dc67c1fbf57b
Scottst123 <scottstroud11@gmail.com>
2023-01-27 21:08:46 -0500

    'python'

--- push.py
+++ push.py
@@ -1,5 +1,11 @@
 import subprocess
 
 bashCommand = "git add ."
+bashCommand2 = "git commit -m 'python'"
+bashCommand3 = "git push -u origin master"
 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+output, error = process.communicate()
+process = subprocess.Popen(bashCommand2.split(), stdout=subprocess.PIPE)
+output, error = process.communicate()
+process = subprocess.Popen(bashCommand3.split(), stdout=subprocess.PIPE)
 output, error = process.communicate()
