1ddbc440f1814292850d31a8267bf32ee651eaf2
Scottst123 <scottstroud11@gmail.com>
2023-01-27 20:59:23 -0500

    python

---
+++ main.py
@@ -0,0 +1,17 @@
+import argparse
+import hashlib
+
+parser = argparse.ArgumentParser(description='Hasing giving password') 
+parser.add_argument('password', help='Password to hash')
+parser.add_argument('-a', '--type', default='sha256', choices=['sha256', 'sha512', 'md5'])
+args = parser.parse_args()
+
+
+password = args.password 
+hashtype = args.type
+m = getattr(hashlib, hashtype)()
+m.update(password.encode())
+
+
+print("< hash-type : " + hashtype + " >")
+print(m.hexdigest())
